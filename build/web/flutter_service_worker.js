c57bdefd14a74e296f8a13ef0e75bdd6059a41b4
Lino Shaji <linoshaji23@gmail.com>
2023-02-21 23:11:20 +0530

    updates on images uploaded

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -44,9 +44,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8b011cc7512043f96ecb764a60feb0b1",
-"/": "8b011cc7512043f96ecb764a60feb0b1",
-"main.dart.js": "8d1781bab137a9d90b8ab9f8576ba592",
+"index.html": "9c3e0d998abd0b0226511beac10b6c53",
+"/": "9c3e0d998abd0b0226511beac10b6c53",
+"main.dart.js": "6db3036aabcdaa6d27ed08483f700ae4",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
